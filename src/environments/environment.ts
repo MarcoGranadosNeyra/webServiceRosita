01d622ce8d0df827c8e03e6225a83e60b9e49baa
Marco Granados <66982998+MarcoGranadosNeyra@users.noreply.github.com>
2022-07-09 08:25:18 -0500

    ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   cloudinary_url: 'https://api.cloudinary.com/v1_1/sistemas-granados/image/upload',
   upload_preset: 'd9egggmx',
-  apiUrl: "34.132.85.115/api/v1/cevicheriarosita/",
-  //apiUrl: "http://192.168.90.4:3333/api/v1/cevicheriarosita",
+  //apiUrl: "34.132.85.115/api/v1/cevicheriarosita/",
+  apiUrl: "http://34.132.85.115/api/v1/cevicheriarosita",
   enableDebug: false
 };
 
